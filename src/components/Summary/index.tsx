20e23e0f4673e64c13881071861c2d0e7765d250
Caio Tracera <ctrcra@gmail.com>
2022-09-19 21:49:15 -0300

    feat(hooks): add useSummary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,28 +3,10 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import { useTransactions } from '@/contexts/TransactionsContext';
 import * as S from '@/components/Summary/styles';
 import { priceFormatter } from '@/utils/formatter';
+import { useSummary } from '@/hooks/useSummary';
 
 export function Summary() {
-  const { transactions } = useTransactions();
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const { summary } = useSummary();
 
   return (
     <S.Wrapper>
